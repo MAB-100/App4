648c86287ffa06991a5b00dba47e5a7d051fd54b
MAB-100 <shahabbas661@gmail.com>
2024-07-23 18:14:55 +0500

    loaded xlxs file in df in main

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import glob
 
-filepaths = glob.glob('data/*.csv')
+filepaths = glob.glob('invoices/*.xlsx')
+print(filepaths)
 
+for filpath in filepaths:
+    df = pd.read_excel(filpath, sheet_name='Sheet 1')
+    print(df)
