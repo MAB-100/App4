1d5f26db8ed4d6411054cc0476d6699258a7fcb5
MAB-100 <shahabbas661@gmail.com>
2024-07-23 19:08:54 +0500

    added total and logos

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ for filpath in filepaths:
     columns = [item.replace('_', ' ').title() for item in columns]
 
     pdf.set_font(family='Times', size= 12, style='B')
-    pdf.set_text_color(80, 80, 80)
     pdf.cell(w = 30, h = 8, txt = columns[0], border=1)
     pdf.cell(w = 70, h = 8, txt = columns[1], border=1)
     pdf.cell(w = 40, h = 8, txt = columns[2], border=1)
@@ -44,5 +43,24 @@ for filpath in filepaths:
         pdf.cell(w = 30, h = 8, txt = str(row['price_per_unit']), border=1)
         pdf.cell(w = 30, h = 8, txt = str(row['total_price']), border=1, ln=1)
 
-    
+    # total row
+    total = df["total_price"].sum()
+    pdf.set_font(family='Times', size= 12)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w = 30, h = 8, txt = "", border=1)
+    pdf.cell(w = 70, h = 8, txt = "", border=1)
+    pdf.cell(w = 40, h = 8, txt = "", border=1)
+    pdf.cell(w = 30, h = 8, txt = "", border=1)
+    pdf.cell(w = 30, h = 8, txt = str(total), border=1, ln=1)
+
+    # total sum sentence
+    pdf.set_font(family='Times', size= 12, style='B')
+    pdf.cell(w = 30, h = 8, txt =f"The toal price is {total}", ln=1)
+
+    # company name and logo
+    pdf.set_font(family='Times', size= 12, style='B')
+    pdf.cell(w = 20, h = 8, txt =f"CureMD")
+    pdf.image("logo.png", w=10)
+
+
     pdf.output(f"PDFs/{filename}.pdf") 
